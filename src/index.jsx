f076e08625c76925a8ba2b5394c69a1956df04fc
lianghancong <lianghancong@aiyunxiao.com>
2018-11-20 11:23:34 +0800

    路由表配置

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter} from 'react-router-dom'
 import { renderRoutes } from 'react-router-config'
 import {routes} from '../router/routeTest';
 
